0e18c4fd397d18884fed5c932e24c33a943052d4
Penny 彭业 <penny@hellotalk.com>
2022-01-17 13:32:22 +0800

    sol 版本报错

--- contracts/Buying.sol
+++ contracts/Buying.sol
@@ -1,2 +1,15 @@
-pragma solidity  ^0.7.0;
+pragma solidity  >=0.4.22 <0.9.0;
 
+contract Buying{
+    address[16] public buyers;
+
+    function buy(uint prodId) public returns (uint){
+        require(prodId >=0 && prodId <=100);
+        buyers[prodId] = msg.sender;
+        return prodId;
+    }
+
+    function getBuyers() public view returns (address[16] memory){
+        return buyers;
+    }
+}
